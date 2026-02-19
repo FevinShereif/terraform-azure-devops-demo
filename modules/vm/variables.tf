acf401c1d7d183c412e89477f02766b4ae1810c9
FevinShereif <shereiffevin51@gmail.com>
2026-02-19 12:52:45 +0530

    Sync local fixes before merge

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,9 +1,10 @@
 variable "location" {}
 variable "vm_name" {}
+
 variable "allowed_vm_sizes" {
-  description = "List of allowed VM sizes"
+  description = "Allowed VM sizes for free subscription"
   type        = list(string)
-  default     = ["Standard_B1s", "Standard_B2s"]  # Free subscription defaults
+  default     = ["Standard_B1s", "Standard_B2s"]
 }
 
 variable "vm_size" {
@@ -15,8 +16,10 @@ variable "vm_size" {
     error_message = "This subscription allows only the following VM sizes: ${join(", ", var.allowed_vm_sizes)}"
   }
 }
+
 variable "admin_username" {}
 variable "admin_password" {}
+
 variable "os_type" {
   description = "Operating system type"
   type        = string
@@ -26,6 +29,7 @@ variable "os_type" {
     error_message = "os_type must be either 'windows' or 'linux'."
   }
 }
+
 variable "tags" {
   description = "Common tags to apply to all resources"
   type        = map(string)
