30b48a0c355568e55a43c5f595db9f2c1ebbe861
FevinShereif <shereiffevin51@gmail.com>
2026-03-13 21:37:01 +0530

    Enable manual workflow run

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,20 +1,9 @@
 variable "location" {}
 variable "vm_name" {}
 
-variable "allowed_vm_sizes" {
-  description = "Allowed VM sizes for free subscription"
-  type        = list(string)
-  default     = ["Standard_B1s", "Standard_B2s"]
-}
-
 variable "vm_size" {
   description = "Azure VM size"
   type        = string
-
-  validation {
-    condition     = contains(var.allowed_vm_sizes, var.vm_size)
-    error_message = "This subscription allows only the following VM sizes: ${join(", ", var.allowed_vm_sizes)}"
-  }
 }
 
 variable "os_type" {
